5cc77f1b91386e0a13914e64d8c0d244b612e7a4
ZongChi1234567 <71251155+ZongChi1234567@users.noreply.github.com>
2020-12-28 14:14:08 +0800

    Add files via upload

---
+++ week16 張宗棨 隨堂練習.py
@@ -0,0 +1,36 @@
+print(True)
+print(False)
+
+print(bool(1))
+print(bool(0))
+print(bool(()))
+
+print(False + 1)
+score = 66
+score == 66
+
+name = "Francis"
+name == "Francis"
+name == "francis"
+
+5>2
+5 <2
+5 !=2
+
+True & True
+True & False
+False & False
+False & True
+True and False
+True or False
+False or True
+False| False
+not True
+not False
+
+X = 70
+print((X > 60) and (X < 80))
+print((X >60) or (X < 80))
+
+"H" in "Hello world"
+"h" in "Hello world"
